a844f32c3fd5f88f263eb834dc7a59706738d81f
Leo Iannacone <l3on@ubuntu.com>
2014-06-16 15:49:11 +0200

    create mergeconfig which given an architecture and a port returns a common debomatic-webui configuration

--- config/amd64.config.js
+++ config/amd64.config.js
@@ -1,30 +1,10 @@
-
 /*
  * debomatic-webui amd64 configuration
  */
 
-var architecture = 'amd64'
-var port = 8064
+var architecture = 'amd64';
+var port = 8064;
 
-
-// common config
-var config = {}
-config.host = 'localhost'
-config.port = port
-
-config.web = {}
-config.web.debomatic = {}
-config.web.debomatic.admin = {}
-config.web.debomatic.admin.email = "dktrkranz AT debian DOT org" // please use this SPAMFREE form - it will be converted client side by javascript
-config.web.debomatic.architecture = architecture
-
-config.debomatic = {}
-config.debomatic.path = '/srv/debomatic-' + architecture
-config.debomatic.jsonfile = '/var/log/debomatic-' + architecture + '.json'
-
-config.web.debomatic.dput = {}
-config.web.debomatic.dput.incoming = config.debomatic.path
-config.web.debomatic.dput.host = "debomatic-" + architecture + ".debian.net"
 
 // DO NOT EDIT THIS LINE:
-module.exports = config
+module.exports = require('./mergerconfig.js')(architecture, port);
--- config/armel.config.js
+++ config/armel.config.js
@@ -1,30 +1,10 @@
-
 /*
  * debomatic-webui armel configuration
  */
 
-var architecture = 'armel'
-var port = 8001
+var architecture = 'armel';
+var port = 8001;
 
-
-// common config
-var config = {}
-config.host = 'localhost'
-config.port = port
-
-config.web = {}
-config.web.debomatic = {}
-config.web.debomatic.admin = {}
-config.web.debomatic.admin.email = "dktrkranz AT debian DOT org" // please use this SPAMFREE form - it will be converted client side by javascript
-config.web.debomatic.architecture = architecture
-
-config.debomatic = {}
-config.debomatic.path = '/srv/debomatic-' + architecture
-config.debomatic.jsonfile = '/var/log/debomatic-' + architecture + '.json'
-
-config.web.debomatic.dput = {}
-config.web.debomatic.dput.incoming = config.debomatic.path
-config.web.debomatic.dput.host = "debomatic-" + architecture + ".debian.net"
 
 // DO NOT EDIT THIS LINE:
-module.exports = config
+module.exports = require('./mergerconfig.js')(architecture, port);
