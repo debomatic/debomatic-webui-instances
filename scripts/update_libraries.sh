2cb847e21507eabfbd8626cefab5e42b4a068efc
Leo Iannacone <l3on@ubuntu.com>
2014-08-26 11:18:47 +0200

    install coffee-script

--- scripts/update_libraries.sh
+++ scripts/update_libraries.sh
@@ -4,6 +4,9 @@ for i in proxy debomatic-webui ; do
   cd $i
   rm -fr node_modules
   npm install
+  if [ "$i" == "debomatic-webui" ] ; then
+    npm install coffee-script
+  fi
   git add -f node_modules
   cd -
 done
